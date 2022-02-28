5de0be2963cf205749120a92e0604ee6a435e67e
Saigunas <saigunasb777@gmail.com>
2022-02-28 09:31:16 +0200

    point values change, game restarts.

--- rps.js
+++ rps.js
@@ -3,20 +3,39 @@ let p1Score = 0;
 let p2Score = 0;
 let numberGamesToWin = 5;
 
+
+
 const textWinAnnounce = document.querySelector('.winnerAnnounce');
 const textPcResult = document.querySelector('.pcResult');
 const textPlayerResult = document.querySelector('.playerResult');
 
 const cards = document.querySelectorAll('.pCard');
 
-cards.forEach(pCard => pCard.addEventListener('click', 
-  playerPlay));
+
+const restartButton = document.querySelector('.restartButton');
+restartButton.addEventListener('click', startGame);
+
+
+//start the game when page is opened
+startGame();
+
+function startGame() {
+  p1Score = 0;
+  p2Score = 0;
+  restartButton.style.visibility='hidden';
+  textPlayerResult.textContent = `${p1Score} points`;
+  textPcResult.textContent = `${p1Score} points`;
+  textWinAnnounce.textContent = 'First to reach 5 points WINS!';
+  textWinAnnounce.style.fontSize = '10vw';
+
+  cards.forEach(pCard => pCard.addEventListener('click', 
+    playerPlay));
+}
 
 function playerPlay(e) {
   const playerSelection = e.target.getAttribute("data-sign");
   playRound(playerSelection, computerPlay());
 } 
-
 
 //Computer randomly returns rock, paper or scissors
 function computerPlay() {
@@ -24,6 +43,8 @@ function computerPlay() {
   let item = possibleMoves[Math.floor(Math.random()*possibleMoves.length)];
   return item;
 }
+
+
 
 
 //Play one round, announce result
@@ -66,6 +87,7 @@ function announceRoundWinner(result, playerSelection, computerSelection) {
   if(result == 'You lose!') {
 
     p2Score++;
+    textPcResult.textContent = `${p2Score} ${pointOrPoints(p2Score)}`;
     if(p2Score === numberGamesToWin) {
       announceGameWinner();
       return;
@@ -76,6 +98,7 @@ function announceRoundWinner(result, playerSelection, computerSelection) {
   } else if(result == 'You win!') {
 
     p1Score++;
+    textPlayerResult.textContent = `${p1Score} ${pointOrPoints(p1Score)}`;
     if(p1Score === numberGamesToWin) {
       announceGameWinner();
       return;
@@ -89,7 +112,6 @@ function announceRoundWinner(result, playerSelection, computerSelection) {
 }
 
 
-
 function announceGameWinner() {
   textWinAnnounce.style.fontSize = '10vw';
   if(p1Score > p2Score) {
@@ -100,4 +122,14 @@ function announceGameWinner() {
   }
   cards.forEach(pCard => pCard.removeEventListener('click', 
   playerPlay));
+
+  restartButton.style.visibility='visible';
+}
+
+function pointOrPoints(nPoint) {
+  if(nPoint === 1) {
+    return 'point';
+  } else {
+    return 'points';
+  }
 }
